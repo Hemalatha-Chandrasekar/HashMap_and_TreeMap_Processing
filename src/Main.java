6b388cb7d6911f5346383d36f38d56cbf042e3cf
Hemalatha-Chandrasekar <hema.alliswell@gmail.com>
2024-12-22 22:12:05 -0600

    Initial Commit

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+import java.util.TreeMap;
 
 public class Main {
+    public static void main(String[] args) {
+        ExampleOneHashMap exampleOneHashMap = new ExampleOneHashMap();
+        exampleOneHashMap.exampleOneHashMap();
+        exampleOneHashMap.ExampletwoHashMap();
+        exampleOneHashMap.exampleTreemapOne();
+        exampleOneHashMap.exampleTreemapTwo();
+        // Creating a treemap with a customized comparator
+        TreeMap<String, Integer> numbers = new TreeMap<>(new CustomComparator());
+
+        numbers.put("First", 1);
+        numbers.put("Second", 2);
+        numbers.put("Third", 3);
+        numbers.put("Fourth", 4);
+        System.out.println("TreeMap: " + numbers);
+    }
 }
+
